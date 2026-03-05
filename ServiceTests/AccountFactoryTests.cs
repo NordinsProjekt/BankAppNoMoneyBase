71086219dca1af6ba4c3ee11339734960fdd5790
Markus Nordin <belfegorc4@gmail.com>
2026-03-05 12:55:46 +0100

    unittests

---
+++ ServiceTests/AccountFactoryTests.cs
@@ -0,0 +1,104 @@
+﻿using Entities.Accounts;
+using Entities.Types;
+using Services.Factories;
+using Services.Models;
+
+namespace ServiceTests;
+
+public class AccountFactoryTests
+{
+    [Fact]
+    public void AccountFactory_CreateAccount_ShouldReturnBankAccount()
+    {
+        //arrange
+        var accountDetails = new AccountDetails
+        {
+            AccountName = "Test Account",
+            AccountNumber = "11",
+            AccountType = AccountType.BankAccount,
+            StartingBalance = 1000m
+        };
+
+        //act
+        var account = AccountFactory.CreateAccount(accountDetails);
+
+        //assert
+        Assert.IsType<BankAccount>(account);
+    }
+
+    [Fact]
+    public void AccountFactory_CreateAccount_ShouldReturnIskAccount()
+    {
+        //arrange
+        var accountDetails = new AccountDetails
+        {
+            AccountName = "Test Account",
+            AccountNumber = "11",
+            AccountType = AccountType.IskAccount,
+            StartingBalance = 1000m
+        };
+
+        //act
+        var account = AccountFactory.CreateAccount(accountDetails);
+
+        //assert
+        Assert.IsType<IskAccount>(account);
+    }
+
+    [Fact]
+    public void AccountFactory_CreateAccount_ShouldReturnUddevallaAccount()
+    {
+        //arrange
+        var accountDetails = new AccountDetails
+        {
+            AccountName = "Test Account",
+            AccountNumber = "11",
+            AccountType = AccountType.UddevallaAccount,
+            StartingBalance = 1000m
+        };
+
+        //act
+        var account = AccountFactory.CreateAccount(accountDetails);
+
+        //assert
+        Assert.IsType<UddevallaAccount>(account);
+    }
+
+    [Fact]
+    public void AccountFactory_CreateAccount_ShouldReturnGoldAccount()
+    {
+        //arrange
+        var accountDetails = new AccountDetails
+        {
+            AccountName = "Test Account",
+            AccountNumber = "11",
+            AccountType = AccountType.GoldAccount,
+            StartingBalance = 1000m
+        };
+
+        //act
+        var account = AccountFactory.CreateAccount(accountDetails);
+
+        //assert
+        Assert.IsType<GoldAccount>(account);
+    }
+
+    [Fact]
+    public void AccountFactory_CreateAccount_ShouldReturnMillAccount()
+    {
+        //arrange
+        var accountDetails = new AccountDetails
+        {
+            AccountName = "Test Account",
+            AccountNumber = "11",
+            AccountType = AccountType.MillionAccount,
+            StartingBalance = 1000m
+        };
+
+        //act
+        var account = AccountFactory.CreateAccount(accountDetails);
+
+        //assert
+        Assert.IsType<MillionAccount>(account);
+    }
+}
